746aae42fa595ea9f28f2da5b25841a659ac1fea
swadiquesadi <swadiquesadi@gmail.com>
2018-02-19 20:07:59 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -8,13 +8,13 @@ app.use(morgan('combined'));
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
-app.get('/article-one',function(req,ser){
+app.get('/article-one',function(req,res){
      res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
 });
-app.get('/article-two',function(req,ser){
+app.get('/article-two',function(req,res){
     res.send("article-two requisted.article-two will be served");
 });
-app.get('/article-three',function(req,ser){
+app.get('/article-three',function(req,res){
     res.send("article-three requisted.article-three will be served");
 });
 app.get('/ui/style.css', function (req, res) {
