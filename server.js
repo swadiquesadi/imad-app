549b6c97c46852bcd85c558c6420517ffde9434d
swadiquesadi <swadiquesadi@gmail.com>
2018-02-19 18:28:55 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,14 +9,14 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 app.get('/article-one',function(req,ser){
-    ser.send("article-one requisted.article-one will be served");
-})
+    ser.sendFile(path.join(_dirname,'ui','index.html'));
+});
 app.get('/article-two',function(req,ser){
     ser.send("article-two requisted.article-two will be served");
-})
+});
 app.get('/article-three',function(req,ser){
     ser.send("article-three requisted.article-three will be served");
-})
+});
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
