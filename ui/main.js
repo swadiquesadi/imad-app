6aba665270ded9929c13150381d4966ec949d880
swadiquesadi <swadiquesadi@gmail.com>
2018-02-20 17:57:09 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,7 +2,7 @@ console.log('Loaded!');
 var element=document.getElementById('main-text');
 element.innerHTML="new text";
 var img=document.getElementById('madi');
-img.onClick = function() {
+img.onclick = function() {
     img.style.marginLeft = '100px' ;
     console.log('done');
 };
