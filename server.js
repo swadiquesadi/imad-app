ee618b31c2e2afb8857529ba60b6b4b5b92b1d24
swadiquesadi <swadiquesadi@gmail.com>
2018-02-20 20:28:54 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -4,6 +4,16 @@ var path = require('path');
 
 var app = express();
 app.use(morgan('combined'));
+
+
+app.get('/counter',function (req, res) {
+    counter = counter+1;
+    
+    res.send(counter.toString());
+   
+    
+});
+
 var articles={
  'article-one':{
     title:'Article one|swadique',
@@ -57,21 +67,6 @@ var htmlTemplate=`<html>
 </html>`;
 return htmlTemplate;
 }
-app.get('/counter',function (req, res) {
-    counter = counter+1;
-    
-    res.send(counter.toString());
-   
-    
-});
-app.get('/:article',function(req,res){
-    var article=req.params.article;
-     res.send(createTemplate(articles[article]));
-});
-
-
-
-
 
 
 app.get('/:article',function(req,res){
