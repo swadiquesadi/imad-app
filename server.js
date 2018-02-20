114d1734ccc47b450f66b40dd7dbc6559a60ea08
swadiquesadi <swadiquesadi@gmail.com>
2018-02-20 20:20:03 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -4,6 +4,13 @@ var path = require('path');
 
 var app = express();
 app.use(morgan('combined'));
+app.get('/counter',function (req, res) {
+    counter = counter+1;
+    
+    res.send(counter.toString());
+   
+    
+});
 
 var articles={
  'article-one':{
@@ -69,13 +76,7 @@ var counter=0;
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
-app.get('/counter',function (req, res) {
-    counter = counter+1;
-    
-    res.send(counter.toString());
-   
-    
-});
+
 
 
 app.get('/ui/style.css', function (req, res) {
