d443fa5a46974f2fac47f4e26bbba2e7f4407a4a
swadiquesadi <swadiquesadi@gmail.com>
2018-02-20 23:16:25 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,7 +1,7 @@
 var button=document.getElementById("counter");
 var counter=0;
 var span=document.getElementById("count");
-counter.onclick=function(){
+button.onclick=function(){
     counter=counter+1;
     var span=document.getElementById("count");
     span.innerHTML=counter.tostring();
