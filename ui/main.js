6060d1f1c3b0128be7a996f30e98895494136e1f
swadiquesadi <swadiquesadi@gmail.com>
2018-02-20 23:09:06 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -3,6 +3,7 @@ var counter=0;
 var span=documet.getElementById("count");
 counter.onclick=function(){
     counter=counter+1;
+    var span=documet.getElementById("count");
     span.innerHTML=counter.tostring();
     
 }
