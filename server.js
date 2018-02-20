af394c8e0a97c68d91d288953f984a13cb8170ab
swadiquesadi <swadiquesadi@gmail.com>
2018-02-20 19:16:25 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -68,7 +68,10 @@ app.get('/:article',function(req,res){
 var counter=0;
 app.get('/counter',function (req, res) {
     counter=counter+1;
+     console.log("counter start");
     res.send(counter.toString());
+    console.log("counter done");
+    
 });
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
