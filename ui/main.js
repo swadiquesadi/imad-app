050ad7b7f1b4577602971d3dfeec69ef2747d0e5
swadiquesadi <swadiquesadi@gmail.com>
2018-02-21 21:17:57 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,8 +2,18 @@ var button=document.getElementById("counter");
 var counter=0;
 var span=document.getElementById("count");
 button.onclick=function(){
-    counter=counter+1;
-    var span=document.getElementById("count");
-    span.innerHTML=counter.toString();
-    
-}
+var request=new XMLHttpRequest();
+request.onreadystatechange = function(){
+                                        if(request.readyState === XMLHttpRequest.DONE)
+                                         {
+                                            if(request.status===200)
+                                             {
+                                                 var counter = request.responseText;
+                                                 var span = document.getElementById("count");
+                                                span.innerHTML=counter.toString();
+                                                }
+                                         }
+                                        }
+request.open('GET',"http://http://swadiquesadi.imad.hasura-app.io/",true);
+request.send(null);
+};
