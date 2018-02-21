58d688600272201745e9a3d5940f49db3b8ab674
swadiquesadi <swadiquesadi@gmail.com>
2018-02-22 00:35:46 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -23,8 +23,9 @@ submit.onclick=function(){
     var names=['name1','name2','name3','name4'];
     var list='';
     for(var i=0;i<names.length;++i)
-        {
+        { 
             list+='<li>'+names[i]+'</li>';
+            console.log("loaded");
         }
     var ul=document.getElementById('namelist');
     ul.innerHTML=list;
