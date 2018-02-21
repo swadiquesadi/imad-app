c546a9df48795412522fbacce29a3458ce1aa899
swadiquesadi <swadiquesadi@gmail.com>
2018-02-22 00:46:29 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -72,7 +72,12 @@ app.get('/counter',function (req, res) {
    
     
 });
-
+var names[];
+app.get('/submit-name/:name',function(req,res){
+    var name=req.params.name;
+    names.push(name);
+    res.send(JSON.stringify(names));
+});
 
 
 
