262e45d06680a0ebf2893676ba3008e83d294c05
swadiquesadi <swadiquesadi@gmail.com>
2018-02-21 21:59:10 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -64,7 +64,7 @@ return htmlTemplate;
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
-
+var counter=0;
 app.get('/counter',function (req, res) {
     counter = counter+1;
     
