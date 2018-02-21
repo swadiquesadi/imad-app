90132ea1cdfaf708ce5097a72f3f8549a06256ef
swadiquesadi <swadiquesadi@gmail.com>
2018-02-22 01:06:46 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -96,8 +96,8 @@ app.get('/:article',function(req,res){
      res.send(createTemplate(articles[article]));
 });
 var names=[];
-app.get('/submit-name/:name',function(req,res){
-    var name=req.params.name;
+app.get('/submit-name',function(req,res){
+    var name=req.query.name;
     names.push(name);
     res.send(JSON.stringify(names));
 
