9f4e434f29838ac61da08006669a6e8a19f45e55
swadiquesadi <swadiquesadi@gmail.com>
2018-02-25 17:18:24 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -106,7 +106,7 @@ app.get('/submit-name',function(req,res){
     res.send(JSON.stringify(names));
 
 });
-app.get('/article/:articleName',function(req,res){
+app.get('/articles/:articleName',function(req,res){
     pool.query("SELECT * FROM article WHERE title='"+req.params.articleName+"'",function(err,result){
     if(err)
         {
