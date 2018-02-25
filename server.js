bc6cada0515450dba399f63eb6a4ae5c32f16054
swadiquesadi <swadiquesadi@gmail.com>
2018-02-25 15:07:35 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app.use(morgan('combined'));
 var config={
     user:'swadiquesadi',
     database:'swadiquesadi',
-    host:'http://db.imad.hasura-app.io',
+    host:'db.imad.hasura-app.io',
     port:'5432',
     password: process.env.DB_PASSWORD
     };
