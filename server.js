e9384dd8a0b833f669b04f2df0a3d66a7b78d416
swadiquesadi <swadiquesadi@gmail.com>
2018-02-25 14:33:41 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -1,9 +1,27 @@
 var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
-
+var Pool=require('pg').Pool;
 var app = express();
 app.use(morgan('combined'));
+var config={
+    username:'swadiquesadi',
+    host:'http://db.imad.hasura-app.io',
+    port:'5432',
+    password:process.env.DB_PASSWORD
+    };
+var pool=new Pool(config);
+app.get('/test-db',function(req,res){
+    pool.query('SELECT * FROM test',function(err,result){
+    if(err){
+        res.status(500).send(err.toString());
+    }  
+    else{
+        res.send(JSON.stringify(result));
+    }
+    });
+    
+});
 var articles={
  'article-one':{
     title:'Article one|swadique',
@@ -68,16 +86,6 @@ app.get('/counter',function (req, res) {
    
     
 });
-
-
-
-
-
-
-
-
-
-
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
@@ -95,18 +103,12 @@ app.get('/submit-name',function(req,res){
     res.send(JSON.stringify(names));
 
 });
-
-
-
 app.get('/:article',function(req,res){
     var article=req.params.article;
      res.send(createTemplate(articles[article]));
 });
-
-
 // Do not change port, otherwise your app won't run on IMAD servers
 // Use 8080 only for local development if you already have apache running on 80
-
 var port = 80;
 app.listen(port, function () {
   console.log(`IMAD course app listening on port ${port}!`);
