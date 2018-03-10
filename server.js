25033da219e07d6f4dd13c9c47fe234bb0159f63
swadiquesadi <swadiquesadi@gmail.com>
2018-03-10 14:45:55 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -111,7 +111,7 @@ app.get('/submit-name',function(req,res){
 function hash(input,salt){
     var hashed=crypto.pbkdf25ync(input,salt,10000,512,'sha512');
     return hashed.toString('hex');
-}
+};
 app.get('/hash/:input',function(req,res){
     var hashedString=hash(req.params.input,'this-is-a-random string');
     res.send(hashedString);
