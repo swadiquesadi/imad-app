9febc2fce2162c8c4812ddb225b41f77bba21ad0
swadiquesadi <swadiquesadi@gmail.com>
2018-03-17 17:37:54 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -28,7 +28,7 @@ var password=document.getElementById('password').value;
 console.log(username);
 console.log(password);
 request.open('POST','http://swadiquesadi.imad.hasura-app.io/login',true);
-request.setRequestHeader('Content-Type','application/json')
+request.setRequestHeader('Content-Type','application/json');
 request.send(JSON.stringify({username:username,password:password}));
 };
 
