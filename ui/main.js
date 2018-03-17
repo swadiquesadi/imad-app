fd4531ee96222f048ed902924ee4086bd965f8dd
swadiquesadi <swadiquesadi@gmail.com>
2018-03-17 16:12:40 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,53 +1,34 @@
 console.log("loaded");
-var button=document.getElementById('counter');
-button.onclick=function(){
-var request=new XMLHttpRequest();
-request.onreadystatechange = function(){
-                                        if(request.readyState === XMLHttpRequest.DONE)
-                                         {
-                                            if(request.status===200)
-                                             {
-                                                 var counter = request.responseText;
-                                                 var span = document.getElementById('count');
-                                                span.innerHTML=counter.toString();
-                                                }
-                                         }
-                                        };
-request.open('GET','http://swadiquesadi.imad.hasura-app.io/counter',true);
-request.send(null);
-};
-
-
 var submit=document.getElementById('submit_btn');
 submit.onclick=function()
 {
 var request=new XMLHttpRequest();
 request.onreadystatechange= function()
-{
-if(request.readyState===XMLHttpRequest.DONE)
+    {
+    if(request.readyState===XMLHttpRequest.DONE)
             {
                 if(request.status===200)
                 {
-                      var names=request.responseText;
-                      names=JSON.parse(names);
-                         var list='';
-                         for(var i=0;i<names.length;++i)
-                        { 
-                         list+='<li>'+names[i]+'</li>';
-                        }
-                       var ul=document.getElementById('namelist');
-                        ul.innerHTML=list;
-                        console.log("work1");
+                     console.log("login successfully");
+                     alert("successfully logged");
                 }
+                else if(request.status===403)
+                    {
+                        console.log("username/password incorrect");
+                        alert("username or password incorrect");
+                    }
+                else if(request.status===500)
+                    {
+                        alert("something went wrong");
+                    }
             }
-   }
-   
-var nameInput=document.getElementById('name');
-var name=nameInput.value;
-
-  request.open('GET','http://swadiquesadi.imad.hasura-app.io/submit-name?name='+ name,true);
-request.send(null);
+    };
+var username=document.getElementById('username').value;
+var password=document.getElementById('password').value;
+console.log(username);
+console.log(password);
+request.open('POST','http://swadiquesadi.imad.hasura-app.io/login',true);
+request.setRequestHeader('Content-Type','application/json')
+request.send(JSON.stringify({username:username,password:password}));
 };
-
-console.log("work 2");
 
